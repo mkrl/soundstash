0f7261bf7b5545b8601b4c6e11a96b20772cbdfd
Mikhail Korolev <self@mkrl.xyz>
2019-07-29 13:01:32 +0800

    Query and pagination

--- src/actions/apiActions.js
+++ src/actions/apiActions.js
@@ -1,6 +1,24 @@
-export function queryText(text) {
+import axios from 'axios'
+
+export function queryText(text, passedOffset = 0) {
+  return function (dispatch) {
+    dispatch({ type: "FETCH_API" })
+    axios.get(`http://musicbrainz.org/ws/2/release?query=${text}&offset=${passedOffset}&limit=12&fmt=json`)
+      .then((resp) => {
+        dispatch({
+          type: "FETCH_API_SUCCESS",
+          payload: {
+            releases: resp.data["releases"],
+            count: resp.data["count"],
+            offset: resp.data["offset"],
+          }
+        })
+      })
+  }
+}
+
+export function clearSearch() {
   return {
-    type: "FETCH_API",
-    payload: text,
+    type: "FETCH_CLEAR"
   }
 }
--- src/components/layout/ListItem.js
+++ src/components/layout/ListItem.js
@@ -1,9 +1,17 @@
 import React from 'react'
 import styled from "styled-components"
 
-const ListItem = styled.li`
+const Item = styled.li`
   background-color: grey;
   color: white;
 `
+
+const ListItem = props => {
+  return (
+    <Item>
+      {props.title} by {props.authors}
+    </Item>
+  )
+}
 
 export default ListItem
